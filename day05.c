c9324edc7e6446c66066e1e19972913a8379ccfd
Kanika <KANIKA.38993@stu.upes.ac.in>
2026-09-10 15:44:52 +0530

    added day 05 program

---
+++ day05.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main() {
+    float celcius, fahrenheit;
+    printf("enter temperature in celcius");
+    scanf("%f", &celcius);
+    fahrenheit = (celcius * 9 /5) + 32;
+    printf("temperature in fahrenheit is %f", fahrenheit);
+    return 0;
+}
